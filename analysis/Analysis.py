25c6f9d813c8c23024526014a8dce4a05eb22cf1
Melissa Chung <melissachung888@gmail.com>
2021-02-11 22:03:42 +0000

    Assigned diff paths to variables -  easy switching

--- analysis/Analysis.py
+++ analysis/Analysis.py
@@ -100,8 +100,12 @@ class Analysis():
         #Knee/Elbow point should be the point with max. absolute second derivative
 
 
+analysisNew = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis New\\"
+analysisOriginal = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original\\"
+analysisOriginalTime = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original Time\\"
+
 '''Finds all files with file extension .mpr and appends them to a list called mpr_files. The sorted() function arranges them in alphabetical order.'''
-mpr_files = sorted(glob.glob('C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original\\*.mpr')) 
+mpr_files = sorted(glob.glob(analysisOriginal + '*.mpr')) 
 #print('\n'.join(mpr_files))
 
 
