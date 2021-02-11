13e07c8cac1394a6a3131160339cb2cc93c95e40
Melissa Chung <melissachung888@gmail.com>
2021-02-11 22:13:48 +0000

    Improved, added better prints and docstring

--- analysis/Current.py
+++ analysis/Current.py
@@ -9,14 +9,25 @@ import matplotlib.ticker as ticker
 from matplotlib.ticker import FormatStrFormatter
 from numpy import diff
 
+'''
+Finds final recorded time for sample set, and returns the two nearest data points (time and current) to 64800 seconds.
 
-mpr_files = sorted(glob.glob('C:\\Users\\User\\Documents\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original Time\\*.txt'))
+(Unfortunately, I cannot remember why this length of time was chosen, and may have been arbritary/a parameter)
+'''
+
+# analysisNew = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis New\\"
+# analysisOriginal = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original\\"
+analysisOriginalTime = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis Original Time\\"
+
+mpr_files = sorted(glob.glob(analysisOriginalTime + '*.txt'))
 
 for i, eChemFile in enumerate(mpr_files):
     df = pd.read_csv(eChemFile, sep='\t', skiprows=0, header=0)
     df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
+    finaltime = df['time/s'].iloc[-1]
 
-    print(os.path.basename(eChemFile))
-    print(df['time/s'].iloc[-1])
+    print("\n \n" + os.path.basename(eChemFile))
+    print("Final time recorded is: ", finaltime, "s, or ", finaltime/3600, "hr")
     closest = df.iloc[(df['time/s']-64800).abs().argsort()[:2]]
+    print("Closest measured time(s) to 64800s, with corresponding currents: ")
     print(closest)
