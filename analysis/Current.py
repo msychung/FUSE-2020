12f7b4589d09ceda744a970fe7ae8aa3be270a5e
Melissa Chung <melissachung888@gmail.com>
2021-02-14 21:12:50 +0000

    Added docstrings etc.

--- analysis/Current.py
+++ analysis/Current.py
@@ -12,7 +12,7 @@ from numpy import diff
 '''
 Finds final recorded time for sample set, and returns the two nearest data points (time and current) to 64800 seconds.
 
-(Unfortunately, I cannot remember why this length of time was chosen, and may have been arbritary/a parameter)
+(Unfortunately, I cannot remember why this length of time was chosen, and may have been arbritary, or a parameter to be changed)
 '''
 
 # analysisNew = "C:\\Users\\Melissa\\OneDrive - Lancaster University\\Jobs & Internships\\FUSE 2020\\Data Analysis\\Analysis\\Analysis New\\"
@@ -23,8 +23,8 @@ mpr_files = sorted(glob.glob(analysisOriginalTime + '*.txt'))
 
 for i, eChemFile in enumerate(mpr_files):
     df = pd.read_csv(eChemFile, sep='\t', skiprows=0, header=0)
-    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]
-    finaltime = df['time/s'].iloc[-1]
+    df = df.loc[:, ~df.columns.str.contains('^Unnamed')]        #gets rid of the Unnamed column
+    finaltime = df['time/s'].iloc[-1]   #finds final value in the time column 
 
     print("\n \n" + os.path.basename(eChemFile))
     print("Final time recorded is: ", finaltime, "s, or ", finaltime/3600, "hr")
