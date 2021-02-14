4eb5329aedba148f7f9e77f9753a5ff07c011acd
Melissa Chung <melissachung888@gmail.com>
2021-02-14 22:41:58 +0000

    More typos corrected

--- analysis/Plot.py
+++ analysis/Plot.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.ticker as ticker
 from numpy import matlib
 
-'''Plotting all initial results next to each other. Passed in resultd manually due to small data pool.'''
+'''Plotting all initial results next to each other. Passed in results manually due to small data pool.'''
 
 fig = plt.figure()
 fig.set_size_inches(10, 6)
